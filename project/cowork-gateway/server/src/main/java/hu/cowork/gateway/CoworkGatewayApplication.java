ae672db83af0f03cc7bb3099b9f3dd4aada159eb
SzAndras1 <szbandras2@gmail.com>
2023-03-09 00:04:27 +0100

    Fix GatewayApplication

--- project/cowork-gateway/server/src/main/java/hu/cowork/gateway/CoworkGatewayApplication.java
+++ project/cowork-gateway/server/src/main/java/hu/cowork/gateway/CoworkGatewayApplication.java
@@ -1,12 +1,13 @@
 package hu.cowork.gateway;
 
 import hu.cowork.advertising.AdvertisingApiClient;
+import hu.cowork.comment_service.CommentApiClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackageClasses = {AdvertisingApiClient.class})
+@EnableFeignClients(basePackageClasses = {AdvertisingApiClient.class, CommentApiClient.class})
 public class CoworkGatewayApplication {
 
     public static void main(String[] args) {
