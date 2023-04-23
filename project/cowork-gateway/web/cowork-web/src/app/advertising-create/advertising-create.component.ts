9c0f1b905736b918502f8933c72671d81a925a5e
SzAndras1 <szbandras2@gmail.com>
2023-04-23 23:54:55 +0200

    Refactor ad create form

--- project/cowork-gateway/web/cowork-web/src/app/advertising-create/advertising-create.component.ts
+++ project/cowork-gateway/web/cowork-web/src/app/advertising-create/advertising-create.component.ts
@@ -2,8 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AdvertisingDto, AdvertisingService} from "../generated";
 import {Router} from "@angular/router";
 import {Location} from "@angular/common";
-import {AdvertisingFormComponent} from "../advertising-form/advertising-form.component";
-import {FormBuilder, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-advertising-create.component',
@@ -11,7 +10,7 @@ import {FormBuilder, Validators} from "@angular/forms";
   styleUrls: ['./advertising-create.component.scss']
 })
 export class AdvertisingCreateComponent implements OnInit {
-  public advertisingDto: AdvertisingDto;
+  profileForm!: FormGroup;
 
   constructor(
     private advertisingService: AdvertisingService,
@@ -19,9 +18,9 @@ export class AdvertisingCreateComponent implements OnInit {
     private location: Location,
     private fb: FormBuilder) {
   }
+
   public createAd(): void{
-    console.log(this.advertisingDto);
-    this.advertisingService.createAd(this.advertisingDto)
+    this.advertisingService.createAd(this.profileForm.value as unknown as AdvertisingDto)
       .subscribe((data: AdvertisingDto) => {
         console.log(data);
       });
@@ -31,72 +30,14 @@ export class AdvertisingCreateComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
-  ngOnInit() {
-    this.advertisingDto = {address: "", email: "", priority: 0, text: "", userId: 0};
-  }
-  profileForm = this.fb.group({
-    userId: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-    email: ['', [Validators.required, Validators.email]],
-    text: ['', [Validators.required, Validators.maxLength(1000)]],
-    address: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
-    priority: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
-  });
 
-  getUserIdErrorMessage(): string {
-    if (this.profileForm.controls.userId.hasError('required')) {
-      return 'You must enter a value';
-    }
-    if (this.profileForm.controls.userId.hasError('pattern')) {
-      return 'Incorrect pattern';
-    }
-
-    return this.profileForm.controls.userId.hasError('required') ? 'Not a valid userId' : '';
-  }
-
-  getEmailErrorMessage(): string {
-    if (this.profileForm.controls.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-    if (this.profileForm.controls.email.hasError('email')) {
-      return 'Incorrect pattern';
-    }
-
-    return this.profileForm.controls.email.hasError('required') ? 'Not a valid email' : '';
-  }
-
-  getTextErrorMessage(): string {
-    if (this.profileForm.controls.text.hasError('required')) {
-      return 'You must enter a value';
-    }
-    if (this.profileForm.controls.text.hasError('maxlength')) {
-      return 'Maximum 1000 characters';
-    }
-
-    return this.profileForm.controls.text.hasError('required') ? 'Not a valid text' : '';
-  }
-
-  getAddressErrorMessage(): string {
-    if (this.profileForm.controls.address.hasError('required')) {
-      return 'You must enter a value';
-    }
-    if (this.profileForm.controls.address.hasError('minlength')) {
-      return 'Must be at least 3 characters';
-    }
-    if (this.profileForm.controls.address.hasError('maxlength')) {
-      return 'Maximum 1000 characters';
-    }
-
-    return this.profileForm.controls.address.hasError('required') ? 'Not a valid address' : '';
-  }
-
-  getPriorityErrorMessage(): string {
-    if (this.profileForm.controls.priority.hasError('required')) {
-      return 'You must enter a value';
-    }
-    if (this.profileForm.controls.priority.hasError('pattern')) {
-      return 'Incorrect pattern';
-    }
-
-    return this.profileForm.controls.priority.hasError('required') ? 'Not a valid priority' : '';
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      userId: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      email: ['', [Validators.required, Validators.email]],
+      text: ['', [Validators.required, Validators.maxLength(1000)]],
+      address: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+      priority: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+    });
   }
 }
