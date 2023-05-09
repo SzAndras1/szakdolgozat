6e97b403b31a7807dd0d0bb7922ea6d0e71e8675
SzAndras1 <szbandras2@gmail.com>
2023-05-09 14:27:30 +0200

    Refactor favorites component in UI

--- project/cowork-gateway/web/cowork-web/src/app/advertising-favorites/advertising-favorites.component.ts
+++ project/cowork-gateway/web/cowork-web/src/app/advertising-favorites/advertising-favorites.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {AdvertisingDto, AdvertisingService} from "../generated";
 import {ActivatedRoute, Router} from "@angular/router";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatDialog} from "@angular/material/dialog";
+import {PopUpDialogComponent} from "../pop-up-dialog/pop-up-dialog.component";
 
 @Component({
   selector: 'app-advertising-favourites',
@@ -10,24 +13,44 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class AdvertisingFavoritesComponent implements OnInit {
   constructor(private advertisingService: AdvertisingService,
               private route: ActivatedRoute,
-              private router: Router,) {
+              private router: Router,
+              public dialog: MatDialog,) {
   }
 
-  displayedColumns: string[] = ['id', 'text', 'email', 'detail', 'activation' ,'delete', 'favorite'];
-  advertisings: AdvertisingDto[]= [];
+  displayedColumns: string[] = ['id', 'text', 'email', 'detail', 'activation', 'delete', 'favorite'];
+  advertisings: AdvertisingDto[] = [];
+  activeAdvertisings: MatTableDataSource<AdvertisingDto>;
+  inactiveAdvertisings: MatTableDataSource<AdvertisingDto>;
 
   getAds(): void {
-    this.advertisingService.getFavoriteAds().subscribe(ad => this.advertisings = ad);
+    this.advertisingService.getFavoriteAds().subscribe(
+      (ad: AdvertisingDto[]) => {
+        let actives: AdvertisingDto[] = []
+        let inactives: AdvertisingDto[] = []
+        this.advertisings = ad;
+        this.advertisings.forEach((selectedAd: AdvertisingDto) => {
+          if (selectedAd.isActive) {
+            actives.push(selectedAd);
+            console.log('active:', selectedAd)
+          } else {
+            inactives.push(selectedAd);
+            console.log('inactive:', selectedAd)
+          }
+        });
+        this.activeAdvertisings = new MatTableDataSource(actives);
+        this.inactiveAdvertisings = new MatTableDataSource(inactives);
+      });
   }
 
   detail(id: string): void {
-    this.router.navigate(['advertising',id]);
+    this.router.navigate(['advertising', id]);
   }
 
   delete(id: number): void {
     this.advertisingService.deleteAdvertising(id).subscribe(
       (data: AdvertisingDto) => {
         console.log(data);
+        this.getAds();
       }
     );
   }
@@ -36,6 +59,7 @@ export class AdvertisingFavoritesComponent implements OnInit {
     this.advertisingService.setAdStatus(id).subscribe(
       (data: any) => {
         console.log(data);
+        this.getAds();
       }
     );
   }
@@ -48,7 +72,21 @@ export class AdvertisingFavoritesComponent implements OnInit {
     this.advertisingService.setAdFavoriteStatus(id).subscribe(
       (data: any) => {
         console.log(data);
+        this.getAds();
       }
     );
+  }
+
+  openDialog(id: number) {
+    let dialogRef = this.dialog.open(PopUpDialogComponent, {
+      data: false
+    })
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (res.data === true) {
+        console.log(res.data);
+        this.delete(id);
+      }
+    })
   }
 }
