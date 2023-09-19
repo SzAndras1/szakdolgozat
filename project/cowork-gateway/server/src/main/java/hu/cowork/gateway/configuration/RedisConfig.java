faf08d13a92f250c80b0180f613021b60f170fcf
SzAndras1 <szbandras2@gmail.com>
2023-09-19 23:47:11 +0200

    Add redis

---
+++ project/cowork-gateway/server/src/main/java/hu/cowork/gateway/configuration/RedisConfig.java
@@ -0,0 +1,34 @@
+package hu.cowork.gateway.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories(basePackages = "hu.cowork.gateway.authenticate.repository")
+public class RedisConfig {
+
+    @Value("${REDIS_HOSTNAME}")
+    private String hostName;
+
+    @Value("${REDIS_PASSWORD}")
+    private String password;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(hostName, 17395);
+        redisConfiguration.setPassword(password);
+        return new JedisConnectionFactory(redisConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
